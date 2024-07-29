48881d4ff76151bb54efaf9270c66dee57ec5ebe
permalik <permalik@protonmail.com>
2024-07-28 23:20:54 -0500

    feat impl yyy_id

--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
+// TODO: output json from string input
+// TODO: unit test
+// TODO: functional test
+// TODO: output json from file input
+// TODO: unit test
+// TODO: functional test
+// TODO: pretty print output
+
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -13,11 +22,16 @@ func check(err error) {
 }
 
 func main() {
-	fmt.Println("Starting yyyoink")
-	data, err := os.ReadFile("data/test.json")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Input JSON string: ")
+	// TODO: Check if Scanner more convenient: https://pkg.go.dev/bufio#Reader.ReadString
+	content, err := reader.ReadString('\n')
 	check(err)
+	fmt.Println(content)
 
-	for i := range data {
-		fmt.Println(string(i))
-	}
+	// fmt.Println("Input JSON file: ")
+	// data, err := os.ReadFile("data/test.json")
+	// check(err)
+
+	// os.Stdout.Write(data)
 }
